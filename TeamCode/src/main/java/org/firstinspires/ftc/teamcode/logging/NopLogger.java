d3e892bb2d127ba98909edc352494a49b26ec347
Eddy Meals <edimea211@gmail.com>
2024-01-13 09:12:09 -0800

    Add NopLogger and SpikeAndParkAutoOpMode
    Add SpikeAndParkAutoOpMode which uses
    SpikeAndParkPathPlanner. Note that it is
    currently blind, eliminating most of the utility
    of an intelligent path planner.
    Add NopLogger which will replace TelemetryLogger
    in most production builds.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/logging/NopLogger.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.logging;
+
+/**
+ * Discards logged messages. Useful for production.
+ */
+public class NopLogger {
+  void log(String msg) { } // do nothing
+}
