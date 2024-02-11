723a7823ded502b9abe94142db19ae9923fc10ac
Eddy Meals <edimea211@gmail.com>
2024-02-10 18:00:41 -0800

    Squash merge feature/far-park-auto
    Squash merge branch 'feature/far-park-auto' (HEAD at
    db8097ba3b6612fa7fa0cd0c1ed38f4215ae761f).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/ParkAutoOpMode.java
@@ -0,0 +1,77 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.DriveSystem;
+import org.firstinspires.ftc.teamcode.drive.MecanumDriveSystem;
+import org.firstinspires.ftc.teamcode.logging.TelemetryLogger;
+import org.firstinspires.ftc.teamcode.navigator.BeelineNavigator;
+import org.firstinspires.ftc.teamcode.navigator.RobotNavigator;
+import org.firstinspires.ftc.teamcode.path.Alliance;
+import org.firstinspires.ftc.teamcode.path.ParkPathPlanner;
+import org.firstinspires.ftc.teamcode.path.PathPlanner;
+import org.firstinspires.ftc.teamcode.path.StartingPosition;
+import org.firstinspires.ftc.teamcode.pilot.RobotPilot;
+import org.firstinspires.ftc.teamcode.pilot.SimplePilot;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+
+import javax.vecmath.Matrix3d;
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Vector3d;
+
+/**
+ * Moves to the position of a the randomly placed spike, then parks backstage.
+ * Uses BeelineNavigator and therefore requires a planner that effectively gives waypoints as goals.
+ * (It can't actually see right now, so it'll get stuck waiting for spike transforms. It'll be way
+ * more interesting after feature/vision is merged.)
+ */
+@Autonomous(name="Far Park Auto", group="Iterative OpMode")
+public class ParkAutoOpMode extends OpMode {
+  private ElapsedTime runtime;
+  private TelemetryLogger logger;
+  private DriveSystem driveSystem;
+  private RobotPilot pilot;
+  private RobotNavigator navigator;
+  private PathPlanner pathPlanner;
+  private Matrix4d cameraTransformRS;
+  @Override
+  public void init() {
+    logger = new TelemetryLogger(telemetry);
+    logger.setFlushMode(true);
+    driveSystem = new MecanumDriveSystem(hardwareMap);
+    Matrix3d initialRobotRotationMat = new Matrix3d();
+    initialRobotRotationMat.rotZ(Math.PI / 2);
+    Matrix4d initialRobotTransform = new Matrix4d(initialRobotRotationMat,
+      new Vector3d(1.5 * 0.61, -2.5 * 0.61, 0), 1.0);
+    Matrix4d ftcOrigin = new Matrix4d();
+    ftcOrigin.rotZ(Math.PI);
+    pilot = new SimplePilot(logger, driveSystem, ftcOrigin, initialRobotTransform,
+      AprilTagGameDatabase.getCenterStageTagLibrary());
+    navigator = new BeelineNavigator(logger, pilot);
+    pathPlanner = new ParkPathPlanner(logger, navigator, Alliance.BLUE, StartingPosition.FRONT);
+
+    telemetry.addData("Status", "Initialized");
+    telemetry.update();
+    logger.setFlushMode(false);
+  }
+  @Override
+  public void start() {
+    runtime = new ElapsedTime();
+  }
+  @Override
+  public void loop() {
+    //pilot.updateWithTags(cameraTransformRS, new ArrayList<>());
+    //pathPlanner.acceptSpikeTransforms(new ArrayList<>());
+    if (pathPlanner.tick()) {
+      telemetry.addData("Status", "Finished");
+    } else {
+      telemetry.addData("Status", "Running");
+      telemetry.addData("Runtime", runtime.toString());
+    }
+    pilot.addTelemetry(telemetry);
+    logger.addTelemetry();
+    telemetry.update();
+  }
+}
